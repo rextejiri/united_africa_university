43b1f39358b8f149fee166df8ba0b57623aebc3d
rextejiri <rextejiri@gmail.com>
2021-05-04 12:28:39 -0500

    dynamic menu

--- app/public/wp-content/themes/united_africa_university_theme/functions.php
+++ app/public/wp-content/themes/united_africa_university_theme/functions.php
@@ -6,6 +6,13 @@
         wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
         wp_enqueue_style('university_main_styles', get_stylesheet_uri());
     }
+    function university_features() {
+        register_nav_menu('headerMenuLocation', 'Header Menu Location');
+        register_nav_menu('footerLocationOne', 'Footer Location One');
+        register_nav_menu('footerLocationTwo', 'Footer Location Two');
+        add_theme_support('title-tag');
+    }
 
-    add_action('wp_enqueue_scripts', 'university_files')
+    add_action('wp_enqueue_scripts', 'university_files');
+    add_action('after_setup_theme', 'university_features');
 ?>
